3ba242dd685077d5cfd6869ac9b2bff75b560a70
aafsheena <aanjum8@gatech.edu>
2025-11-03 16:45:24 -0500

    phaseI.py

--- pages/phaseI.py
+++ pages/phaseI.py
@@ -6,7 +6,7 @@ st.title("Lab03 Phase I")
 url = "http://www.omdbapi.com/?apikey=89d15140&"
 
 st.text("---------------------------------")
-
+st.title("🎬 Movie Rating Explorer")
 
 
 st.text("---------------------------------")
@@ -14,3 +14,4 @@ st.text("---------------------------------")
 st.header("Keigo")
 m_data = requests.get(url + "t=Inception").json()
 st.image(m_data["Poster"], width=200)
+
