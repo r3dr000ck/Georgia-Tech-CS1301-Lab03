cded063ebc0d0e912454ac46a4f12f7d0f8b4cc2
r3dr000ck <keigo.momo.momo@gmail.com>
2025-11-03 16:41:16 -0500

    phase1

--- pages/phaseI.py
+++ pages/phaseI.py
@@ -1,5 +1,11 @@
 import requests
+import streamlit as st
+
+st.title("Movie Info Page")
 
 url = "http://www.omdbapi.com/?apikey=89d15140&"
-data = requests.get(url + "t=Inception").json()
-print(data)
+
+
+st.header("Posters")
+m_data = requests.get(url + "t=Inception").json()
+st.image(m_data["Poster"], width=200)
