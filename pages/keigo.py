d9859f4cde3e5eaa5ad21767da52e5988a2439bd
r3dr000ck <keigo.momo.momo@gmail.com>
2025-11-17 15:50:25 -0500

    gemini

--- pages/keigo.py
+++ pages/keigo.py
@@ -1,9 +1,12 @@
 import google.generativeai as genai
+import requests
 
 GEMINI_API_KEY = "AIzaSyCQNy4vivWIVBlvT1hqRzz2VXs5nLuvPMU"
 
 genai.configure(api_key=GEMINI_API_KEY)
 model = genai.GenerativeModel("gemini-2.5-flash")
+
+url = "http://www.omdbapi.com/?apikey=89d15140&"
 
 # Example
 response = model.generate_content("Tell me a joke about lions.")
